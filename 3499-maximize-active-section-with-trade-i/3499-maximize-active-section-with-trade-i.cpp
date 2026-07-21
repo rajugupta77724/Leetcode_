dd83e1f5d276c902658418d510a84fb80551b9d0
rajugupta77724 <141480008+rajugupta77724@users.noreply.github.com>
2026-07-21 14:14:33 +0530

    Time: 181 ms (8.11%), Space: 175.8 MB (11.71%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -6,10 +6,8 @@ public:
         for (char c : s)
             if (c == '1')
                 ones++;
-
         string t = "1" + s + "1";
         int m = t.size();
-
         vector<pair<char, int>> runs;
         for (int i = 0; i < m;) {
             int j = i;
@@ -18,22 +16,17 @@ public:
             runs.push_back({t[i], j - i});
             i = j;
         }
-
         int ans = ones;
-
         for (int i = 1; i + 1 < runs.size(); i++) {
             if (runs[i].first == '1' &&
                 runs[i - 1].first == '0' &&
                 runs[i + 1].first == '0') {
-
                 int removed = runs[i].second;
                 int mergedZero = runs[i - 1].second + removed + runs[i + 1].second;
-
                 int gain = mergedZero - removed;
                 ans = max(ans, ones + gain);
             }
         }
-
         return min(ans, n);
     }
 };
