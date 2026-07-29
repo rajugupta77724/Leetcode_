5fc2e8c6620041ff2c08437bb6c85cbfde1a8286
rajugupta77724 <141480008+rajugupta77724@users.noreply.github.com>
2026-07-29 10:45:00 +0530

    Time: 21 ms (69.56%), Space: 17.3 MB (40.58%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -2,48 +2,31 @@ class Solution {
 public:
     string smallestPalindrome(string s, int k) {
         vector<int> freq(26, 0);
-
         for (char c : s)
             freq[c - 'a']++;
-
-        // Build the left half
         vector<int> half(26);
         char middle = 0;
-
         for (int i = 0; i < 26; i++) {
             half[i] = freq[i] / 2;
-
             if (freq[i] % 2)
                 middle = char('a' + i);
         }
-
         int len = s.size() / 2;
-
-        // Calculate combinations, capped at k
         auto combinations = [&](int n, int r) -> long long {
             r = min(r, n - r);
-
             long long ans = 1;
-
             for (int i = 1; i <= r; i++) {
                 ans = ans * (n - r + i) / i;
-
                 if (ans >= k)
                     return k;
             }
-
             return ans;
         };
-
-        // Count number of distinct permutations
         auto countWays = [&](vector<int>& cnt) -> long long {
             int remaining = 0;
-
             for (int x : cnt)
                 remaining += x;
-
             long long ways = 1;
-
             for (int i = 0; i < 26; i++) {
                 if (cnt[i] == 0)
                     continue;
