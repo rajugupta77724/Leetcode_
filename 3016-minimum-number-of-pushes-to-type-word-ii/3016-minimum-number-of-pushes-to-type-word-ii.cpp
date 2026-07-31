ff1be22bcbc78e51e1e5318b314551b3023838fd
rajugupta77724 <141480008+rajugupta77724@users.noreply.github.com>
2026-07-31 11:44:35 +0530

    Time: 7 ms (69.76%), Space: 25.4 MB (72.47%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> freq(26, 0);
         for (char ch : word)
             freq[ch - 'a']++;
-
         sort(freq.begin(), freq.end(), greater<int>());
         int ans = 0;
         for (int i = 0; i < 26; i++) {
